abf2d52c015cc3ddf0482be21071490a8e331d42
SADMAN AHMED OMEO <sadmanahmed167@gmail.com>
2026-02-14 01:37:38 +0600

    update name in env

--- bhangti_chai_app/lib/main.dart
+++ bhangti_chai_app/lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Bhangti Chai APP',
+      title: 'Bhangti Chai App',
       theme: ThemeData(
         // This is the theme of your application.
         //
